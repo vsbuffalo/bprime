a91e502b694de8404e46e9a031ba47be15d2f1c0
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-03-01 08:08:44 -0800

    updated sims/snakefile

--- slim_sims/snakemake_runner.sh
+++ slim_sims/snakemake_runner.sh
@@ -1,3 +1,7 @@
+for i in {1..100}
+do
 snakemake -j 999 --rerun-incomplete --use-conda --config config="./cluster_talapas.json" --cluster-config cluster_talapas.json \
-   --jobname='bgs_segment.{rulename}.{jobid}.sh' --scheduler greedy \
-   --cluster 'sbatch --error {cluster.error} --output {cluster.output} -A {cluster.account} -p {cluster.partition} -n {cluster.n} -t {cluster.time} --mem-per-cpu {cluster.mem} -c {cluster.cores}' all
+   --jobname='bgs_segment.{rulename}.{jobid}.sh' --scheduler greedy --rerun-incomplete \
+   --cluster 'sbatch --error {cluster.error} --output {cluster.output} -A {cluster.account} -p {cluster.partition} -n {cluster.n} -t {cluster.time} --mem-per-cpu {cluster.mem} -c {cluster.cores}' \
+   --batch all=$i/100 all
+done
