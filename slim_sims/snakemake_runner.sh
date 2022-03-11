9a19e9d2824eb7d969679813c077abeb30e7525c
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-03-11 15:33:09 -0800

    updated snakefile to grow time

--- slim_sims/snakemake_runner.sh
+++ slim_sims/snakemake_runner.sh
@@ -1,7 +1,7 @@
-for i in {1..50}
+for i in {1..10}
 do
 snakemake -j 999 --rerun-incomplete --use-conda --config config="./cluster_talapas.json" --cluster-config cluster_talapas.json \
    --jobname='bgs_segment.{rulename}.{jobid}.sh' --scheduler greedy --rerun-incomplete \
    --cluster 'sbatch --error {cluster.error} --output {cluster.output} -A {cluster.account} -p {cluster.partition} -n {cluster.n} -t {cluster.time} --mem-per-cpu {cluster.mem} -c {cluster.cores}' \
-   --batch all=$i/50 all
+   --batch all=$i/10 all
 done
