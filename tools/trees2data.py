069f90ae2d95e883a55e7ff4f79c6ec24b989f58
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-03-12 20:59:57 -0800

    updated snakemake routine, slim

--- tools/trees2data.py
+++ tools/trees2data.py
@@ -17,8 +17,9 @@ def trees2training_data(dir, features, recap='auto',
         needs_recap = max(t.num_roots for t in ts.trees()) > 1
         if (recap is True) or (recap == 'auto' and needs_recap):
             ts = pyslim.slim_tree_sequence.SlimTreeSequence(ts)
-            rts = pyslim.recapitate(ts, recombination_rate=0,
-                                    ancestral_Ne=md['N'][0]).simplify()
+            ts = pyslim.recapitate(ts, recombination_rate=0,
+                                   ancestral_Ne=md['N'][0]).simplify()
+        nroots = max(t.num_roots for t in ts.trees())
         assert(nroots == 1)
         region_length = int(md['region_length'][0])
         seglen = int(md['seglen'][0])
@@ -36,6 +37,7 @@ def trees2training_data(dir, features, recap='auto',
         X.append(tuple(md[f][0] for f in features))
         # get targets and other data
         y.append((pi[0], 0.25*pi[0]/float(md['N'][0]), Ef, Vf, load))
+    print(np.array(y).mean(axis=0))
     return np.array(X), np.array(y), features
 
 
@@ -44,10 +46,11 @@ def trees2training_data(dir, features, recap='auto',
 @click.option('--outfile', default='B_data',
               type=click.Path(writable=True),
               help='path to save data to (exclude extension)')
+@click.option('--recap', default='auto', help='recapitate trees')
 @click.option('--suffix', default='recap.tree', help='tree file suffix')
 @click.option('--features', default='N,s,h,mu,recfrac,rbp,seglen',
               help='features to extract from metadata')
-def main(dir, outfile, suffix, features):
+def main(dir, outfile, recap, suffix, features):
     X, y, features = trees2training_data(dir, features=features.split(','), suffix=suffix)
     np.savez(outfile, X=X, y=y, features=features)
 
