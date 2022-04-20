d5cae6335fdcfdcdcae6bca45588e6bb1338b816
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-04-19 18:53:15 -0700

    commited run helper

--- msprime_sims/README.md
+++
@@ -1,5 +0,0 @@
-
-
-    bash snakemake_runner.sh -c msprime_bgs_simple.json &&
-    bash snakemake_runner.sh -c msprime_bgs_segment.json &&
-    bash snakemake_runner.sh -c msprime_bgs_simple_varL.json &&bash snakemake_runner.sh -c msprime_bgs_simple_varL_varrbp.json 
---
+++ msprime_sims/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bash snakemake_runner.sh -c msprime_bgs_simple.json &&
+ bash snakemake_runner.sh -c msprime_bgs_segment.json &&
+ bash snakemake_runner.sh -c msprime_bgs_simple_varL.json && 
+ bash snakemake_runner.sh -c msprime_bgs_simple_varL_varrbp.json 
+ bash snakemake_runner.sh -c msprime_bgs_segment_uniform_mu.json
