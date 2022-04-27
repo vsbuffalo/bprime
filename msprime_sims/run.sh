4c5b669b465138a470ba48a838e9c716c60393ee
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-04-26 22:37:39 -0700

    changes to run.sh

--- msprime_sims/run.sh
+++ msprime_sims/run.sh
@@ -5,4 +5,6 @@ bash snakemake_runner.sh -c ./simple_varL.json &&
 bash snakemake_runner.sh -c ./simple_varlogL.json &&
 bash snakemake_runner.sh -c ./simple_varlogL_varrbp.json &&
 bash snakemake_runner.sh -c ./segment_logL_logrbp_logrf.json &&
-bash snakemake_runner.sh -c ./segment_logL_logrbp_logrf_wide.json
+bash snakemake_runner.sh -c ./segment_logL_logrbp_logrf_wide.json &&
+bash snakemake_runner.sh -c ./segment_logL_logrbp_logrf_wide_reject.json &&
+bash snakemake_runner.sh -c ./segment_logL_logrbp_logrf_wide_reject_replicated.json
