fixed subtle off-by-one error in sims

bgspy/sim_utils.py
@@ -195,4 +195,4 @@ def load_b_chrom_sims(dir, progress=True, ncores=None, **kwargs):
 
     mu = np.fromiter(mu_lookup.keys(), dtype=float)
     sh = np.fromiter(sh_lookup.keys(), dtype=float)
-    return mu, sh, pos, X
+    return mu, sh, pos, X, tree_files

tools/process_sims.py
@@ -9,12 +9,12 @@ from bgspy.sim_utils import load_b_chrom_sims
 
 if __name__ == "__main__":
     ncores = 6
-    mu, sh, pos, X = load_b_chrom_sims(sys.argv[1], ncores=ncores)
+    mu, sh, pos, X, files = load_b_chrom_sims(sys.argv[1], ncores=ncores)
 
     nreps = X.shape[3]
     mean = X.mean(axis=3)
     sd = X.std(axis=3)
-    np.savez(sys.argv[2], mu=mu, sh=sh, pos=pos, X=X, nreps=nreps, mean=mean, sd=sd)
+    np.savez(sys.argv[2], mu=mu, sh=sh, pos=pos, X=X, nreps=nreps, mean=mean, sd=sd, files)
 
 
 