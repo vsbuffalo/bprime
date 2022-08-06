ee320ef6ddb23a64851238699307f74e0b89e65f
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2022-08-06 14:09:11 -0700

    working C likelihood

--- src/likelihood.c
+++ src/likelihood.c
@@ -1,36 +1,53 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-#define LOGBW_GET(lB, ii, ll, jj, kk, s) lB[ii*s[0] + ll*s[1] + jj*s[2] + kk*s[3]]
-#define W_GET(theta, jj, kk, nf) theta[2 + nf*jj + kk]
+#define LOGBW_GET(lB, ii, ll, jj, kk, s) lB[(ii)*s[0] + (ll)*s[1] + (jj)*s[2] + (kk)*s[3]]
+#define W_GET(WW, jj, kk, col) WW[col*(jj) + kk]
 
 double interp_logBw(double x, double *w, double *logB, ssize_t nw,
-              ssize_t i, ssize_t j, ssize_t k, ssize_t *logB_strides) {
+              ssize_t i, ssize_t j, ssize_t k, ssize_t *strides) {
     double min_w = w[0];
     double max_w = w[nw-1];
-    printf("interpolation bounds: [%.3g, %.3g]\n", min_w, max_w);
+    //printf("interpolation bounds: [%.3g, %.3g]\n", min_w, max_w);
     // if mutation is weak below threshold, B = 1 so we return log(1) = 0
     if (x < min_w) return 0;
-    assert(x < max_w);
+    if (x > max_w) {
+        printf("ERROR: x=%g out past max (bounds: [%g, %g])\n", x, min_w, max_w);
+    }
     double y1, y2;
-    for (int l=1; l < nw; l++) {
-        if (w[l-1] <= x && x < w[l]) {
-            y1 = LOGBW_GET(logB, i, l-1, j, k, logB_strides);
-            y2 = LOGBW_GET(logB, i, l, j, k, logB_strides);
-            return (y2 - y1) / (w[i] - w[i-1]) * (x - w[i-1]) + y1;
+    for (int l=0; l < nw-1; l++) {
+        if (w[l] <= x && x < w[l+1]) {
+            //assert(l-1 >= 0);
+            //printf("l = %d\n", l);
+            y1 = LOGBW_GET(logB, i, l, j, k, strides);
+            y2 = LOGBW_GET(logB, i, l+1,   j, k, strides);
+            //printf("y1 = %g, y2 = %g\n", y1, y2);
+            double y = (y2 - y1) / (w[l+1] - w[l]) * (x - w[l]) + y1;
+            //printf("y = %g", y);
+            return y;
         }
     }
-    printf("ERROR: interpolation failed: x=%f (bounds: [%f, %f])\n", x, w[0], w[nw-1]);
-    //assert(0);
-    return 1.0;
+    printf("ERROR: interpolation failed: x=%g (bounds: [%g, %g])\n", x, min_w, max_w);
+    assert(0);
+    //return 1.0;
 }
 
 double test(double *test) {
     return test[0];
     printf("%f", test[0]);
     return 1123.8377;
+}
+
+double access(double *logB, ssize_t i, ssize_t l, ssize_t j, ssize_t k,
+              ssize_t *dim, ssize_t *logB_strides) {
+    ssize_t *strides = malloc(4 * sizeof(ssize_t));
+    for (int i=0; i<4; i++) strides[i] = logB_strides[i] / sizeof(double);
+    double out = LOGBW_GET(logB, i, l, j, k, strides);
+    free(strides);
+    return out;
 }
 
 double negloglik(double *theta,
@@ -44,25 +61,39 @@ double negloglik(double *theta,
     ssize_t nw = logB_dim[1];
     ssize_t nt = logB_dim[2];
     ssize_t nf = logB_dim[3];
-    printf("dims: nx=%d, nw=%d, nt=%d, nf=%d\n", nx, nw, nt, nf);
+    //printf("dims: nx=%d, nw=%d, nt=%d, nf=%d\n", nx, nw, nt, nf);
     double pi0 = theta[0];
+    double mu = theta[1];
+    ssize_t nW = (nt-1)*nf;
+    double *W = calloc(nW, sizeof(double));
+    memcpy(W, theta + 2, nW * sizeof(double));
     double *logBw = calloc(nx, sizeof(double));
     double Wjk;
-    double ll;
-    double mu = theta[1];
+    double ll = 0;
+    ssize_t *strides = malloc(4 * sizeof(ssize_t));
+    for (int i=0; i<4; i++) strides[i] = logB_strides[i] / sizeof(double);
+    //for (int i=0; i<4; i++) printf("-> i=%d, %d", i, strides[i]);
 
-    printf("mu: %g\n", mu);
+    //for (int i=0; i < 50; i++) printf("   %g\n", logB[i]);
+
+    //for (int i=0; i < 4; i++) printf(" stide %d=%d \n", i, logB_strides[i]);
+    //printf("mu: %g\n", mu);
 
     ssize_t n_theta = 2 + (nt-1)*nf;
-    for (int i=0; i < n_theta; i++) {
-        printf("theta(i=%d) -> %g\n", i, theta[i]);
-    }
+    /* for (int i=0; i < n_theta; i++) { */
+    /*     printf("theta(i=%d) -> %g\n", i, theta[i]); */
+    /* } */
+
+    /* for (int i=0; i < nW; i++) { */
+    /*     printf("W(i=%d) -> %g\n", i, W[i]); */
+    /* } */
+
 
     double *wsum = calloc(nf, sizeof(double));
     for (int k=0; k < nf; k++) {
         for (int j=0; j < nt-1; j++)
-            wsum[k] += W_GET(theta, j, k, nf);
-        printf("W[%d]=%g\n", k, wsum[k]);
+            wsum[k] += W_GET(W, j, k, nf);
+        //printf("W[%d]=%g\n", k, wsum[k]);
     }
 
     for (int i=0; i < nx; i++) {
@@ -70,17 +101,26 @@ double negloglik(double *theta,
             for (int k=0; k < nf; k++) {
                 if (j == 0) {
                     // this is the fixed class of the simplex
-                    Wjk = 1 - wsum[k];
+                    Wjk = 1. - wsum[k];
                 } else {
-                    Wjk = W_GET(theta, j-1, k, nf);
+                    Wjk = W_GET(W, j-1, k, nf);
+                    //if (j == 4) printf("j=%d, k=%d,offset: %d, nW=%d, W=%g W_GET=%g\n", j, k, nf*(j-1) + k, nW, W[(j-1)*nf + k], Wjk);
                 }
-                printf("mu * Wjk: %g\n", mu * Wjk);
-                logBw[i] += interp_logBw(mu*Wjk, w, logB, nw, i, j, k, logB_strides);
+                //printf("i=%d, j=%d, k=%d | mu=%g, Wjk=%g, mu Wjk=%g\n", i, j, k, 
+                //       mu, Wjk, mu * Wjk);
+                logBw[i] += interp_logBw(mu*Wjk, w, logB, nw, i, j, k, strides);
             }
         }
+        //printf("%g, ", logBw[i]);
         double log_pi = log(pi0) + logBw[i];
-        ll += nD[i]*log_pi + nS[i]*log1p(exp(-log_pi));
+        //printf("c log(pi0): %g\n", log(pi0));
+        //printf("c nD[%d]=%g, nS[%d]=%g\n", i, nD[i], i, nS[i]);
+        //printf("c llm[%d]: %g\n", i, nD[i]*log_pi + nS[i]*log(1 - exp(log_pi)));
+        ll += nD[i]*log_pi + nS[i]*log1p(-exp(log_pi));
     }
+    free(strides);
+    free(wsum);
+    free(logBw);
     return -ll;
 }
 
--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 import ctypes
 from ctypes import POINTER, c_double
+from bgspy.likelihood import negll_numba
 
 lib = np.ctypeslib.load_library('lik', '../src/')
 dat = pickle.load(open('../data/test_data.pkl', 'rb'))
@@ -18,15 +19,52 @@ theta = np.array([1e-3, 1e-8, #0.8, 0.8, 0.1,
 
 assert(theta.size == 2 + (nt-1)*nf)
 
+print("Is contiguous? ", B.flags['C_CONTIGUOUS'])
+print("B dtype: ", B.dtype)
 print("W=\n", theta[2:].reshape((nt-1, nf)))
 print(theta[2:].reshape((nt-1, nf)).sum(axis=0))
 
 nS, nD = np.array(Y[:, 0]), np.array(Y[:, 1])
 
-def negloglik(theta, Y, logB, w):
-    nS = np.require(Y[:, 0], int, ['ALIGNED'])
-    nD = np.require(Y[:, 1], int, ['ALIGNED'])
-    theta = np.require(theta, float, ['ALIGNED'])
+lib.access.restype = c_double
+
+B = np.require(B, float, ['ALIGNED'])
+
+i, l, j, k = 0, 0, 0, 0
+print(f"python: {i},{l},{j},{k}: ", B[i, l, j, k], end='\t//\t')
+B_ptr = B.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+shape, strides = B.ctypes.shape, B.ctypes.strides
+print(f"C: {i},{l},{j},{k}: ", lib.access(B_ptr, i, l, j, k, shape, strides))
+
+print('------')
+print(w.strides)
+
+# import sys
+# sys.exit(0)
+#print(B.flatten()[:50])
+
+for _ in range(10):
+    i = np.random.randint(0, nx)
+    l = np.random.randint(0, nw)
+    j = np.random.randint(0, nt)
+    k = np.random.randint(0, nf)
+    print(f"python: {i},{l},{j},{k}: ", B[i, l, j, k], end='\t//\t')
+    s = B.strides
+    print(f"python manual: {i},{l},{j},{k}: ",
+          B.flat[(s[0]*i + s[1]*l + s[2]*j + s[3]*k) // B.dtype.itemsize], end='\t//\t')
+    B_ptr = B.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    shape, strides = B.ctypes.shape, B.ctypes.strides
+    lib.access.argtypes = (POINTER(c_double), ctypes.c_ssize_t,  ctypes.c_ssize_t,
+                           ctypes.c_ssize_t, ctypes.c_ssize_t, POINTER(ctypes.c_ssize_t),
+                           POINTER(ctypes.c_ssize_t))
+    print(f"C: {i},{l},{j},{k}: ", lib.access(B_ptr, i, l, j, k, shape, strides))
+
+print("Y DTYPE", Y.dtype)
+
+def cnegloglik(theta, Y, logB, w):
+    nS = np.require(Y[:, 0].flat, np.float64, ['ALIGNED'])
+    nD = np.require(Y[:, 1].flat, np.float64, ['ALIGNED'])
+    theta = np.require(theta, np.float64, ['ALIGNED'])
     nS_ptr = nS.ctypes.data_as(POINTER(c_double))
     nD_ptr = nD.ctypes.data_as(POINTER(c_double))
     theta_ptr = theta.ctypes.data_as(POINTER(c_double))
@@ -34,6 +72,23 @@ def negloglik(theta, Y, logB, w):
     w_ptr = w.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
     lib.negloglik.argtypes = (POINTER(c_double), POINTER(c_double), POINTER(c_double),
                              POINTER(c_double), POINTER(c_double), ctypes.POINTER(np.ctypeslib.c_intp), ctypes.POINTER(np.ctypeslib.c_intp))
+    lib.negloglik.restype = c_double
     return lib.negloglik(theta_ptr, nS_ptr, nD_ptr, logB_ptr, w_ptr, logB.ctypes.shape, logB.ctypes.strides)
 
-negloglik(theta, Y, B, w)
+# compare
+# adjust theta to incorporate mu
+mu = theta[1]
+W = theta[2:].reshape((nt-1, nf))
+W_first_row = 1 - W.sum(axis=0)
+W = mu * np.vstack((W_first_row, W))
+print(W)
+
+alt_theta = np.empty((1 + nt*nf))
+alt_theta[0] = theta[0]
+alt_theta[1:] = W.flat
+print(alt_theta)
+print("negll_numba: ", negll_numba(alt_theta, Y, B, w))
+print("cnegloglik: ", cnegloglik(theta, Y, B, w))
+
+
+
