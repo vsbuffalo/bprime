fixed subtle off-by-one error in cummulative recmaps

bgspy/likelihood.py
@@ -24,7 +24,7 @@ from scipy import interpolate
 from scipy.optimize import minimize
 from numba import jit
 from bgspy.utils import signif
-from bgspy.data import pi_from_pairwise_summaries
+from bgspy.data import pi_from_pairwise_summaries, GenomicBins
 from bgspy.optim import run_optims, nlopt_mutation_worker, nlopt_simplex_worker
 
 # load the library (relative to this file in src/)
@@ -419,9 +419,30 @@ class BGSLikelihood:
 
     (note Bs are stored in log-space.)
     """
-    def __init__(self, w, t, logB, Y=None,
-                 log10_pi0_bounds=(-5, -1),
-                 seed=None):
+    def __init__(self,
+                 # data stuff:
+                 bins, Y,
+                 # one way to specify the B tensor is manually
+                 w=None, t=None, logB=None,
+                 # another way:
+                 bscores=None,
+                 log10_pi0_bounds=(-5, -1)):
+        manual_specification = w is not None and t is not None and logB is not None
+        bscores_specification = bscores is not None
+        msg = "either w, t, and logB need to bet set, or bscores"
+        assert manual_specification or bscores_specification, msg
+        assert not (manual_specification and bscores_specification), msg
+        self.bscores = None
+        if bscores_specification:
+            w = bscores.w
+            t = bscores.t
+            # create the B tensor for these bins.
+            assert isinstance(bins, GenomicBins), "bins must be a GenomicBins object"
+            logB = np.concatenate([bscores.bin_means(bins).B[c] for c in bins.bins.keys()], axis=0)
+            self.bscores = bscores
+        else:
+            assert manual_specification
+
         self.w = w
         self.t = t
         self.log10_pi0_bounds = log10_pi0_bounds
@@ -433,18 +454,19 @@ class BGSLikelihood:
         except AssertionError:
             raise AssertionError("logB has incorrection shape, should be nx x nw x nt x nf")
 
-        if Y is not None:
-            try:
-                logB.shape[0] == Y.shape[0]
-                Y.shape[1] == 2
-            except AssertionError:
-                raise AssertionError("Y shape is incorrect")
+        # check the data and bins
+        try:
+            logB.shape[0] == Y.shape[0]
+            Y.shape[1] == 2
+        except AssertionError:
+            raise AssertionError("Y shape is incorrect")
         self.Y = Y
 
         self.logB = logB
+        self.bins = bins
         self.theta_ = None
         self.dim()
-        self.rng = np.random.default_rng(seed)
+        #self.rng = np.random.default_rng(seed)
 
 
     def dim(self):
@@ -598,10 +620,10 @@ def negll_simplex_fixed_mutation_full(theta, grad, Y, B, w, mu):
 
 
 class FreeMutationModel(BGSLikelihood):
-    def __init__(self, w, t, logB, Y=None, log10_pi0_bounds=(-5, -1), seed=None):
-        super().__init__(w=w, t=t, logB=logB, Y=Y,
-                         log10_pi0_bounds=log10_pi0_bounds,
-                         seed=seed)
+    def __init__(self, bins, Y, w=None, t=None, logB=None, bscores=None,
+                 log10_pi0_bounds=(-5, -1)):
+        super().__init__(bins=bins, Y=Y, w=w, t=t, logB=logB, bscores=bscores,
+                         log10_pi0_bounds=log10_pi0_bounds)
 
     def random_start(self):
         """
@@ -695,10 +717,10 @@ class FreeMutationModel(BGSLikelihood):
         return base_rows
 
 class SimplexModel(BGSLikelihood):
-    def __init__(self, w, t, logB, Y=None, log10_pi0_bounds=(-5, -1), seed=None):
-        super().__init__(w=w, t=t, logB=logB, Y=Y,
-                         log10_pi0_bounds=log10_pi0_bounds,
-                         seed=seed)
+    def __init__(self, bins, Y, w=None, t=None, logB=None, bscores=None,
+                 log10_pi0_bounds=(-5, -1)):
+        super().__init__(bins=bins, Y=Y, w=w, t=t, logB=logB, bscores=bscores,
+                         log10_pi0_bounds=log10_pi0_bounds)
 
     def random_start(self):
         """
@@ -787,11 +809,10 @@ class SimplexModel(BGSLikelihood):
         return predict_simplex(theta, self.logB, self.w)
 
 class FixedMutationModel(BGSLikelihood):
-    def __init__(self, w, t, logB, Y=None, log10_pi0_bounds=(-5, -1), seed=None):
-        super().__init__(w=w, t=t, logB=logB, Y=Y,
-                         log10_pi0_bounds=log10_pi0_bounds,
-                         seed=seed)
-
+    def __init__(self, bins, Y, w=None, t=None, logB=None, bscores=None,
+                 log10_pi0_bounds=(-5, -1)):
+        super().__init__(bins=bins, Y=Y, w=w, t=t, logB=logB, bscores=bscores,
+                         log10_pi0_bounds=log10_pi0_bounds)
 
     def random_start(self):
         """

bgspy/recmap.py
@@ -41,9 +41,15 @@ class RecMap(object):
     rate interpolators. Note that a conversion_factor of 1e-8 is the default
     for cM/Mb rates (1cM = 0.01M, 1Mb = 10^6 bases, 0.01 / 1e6 = 1e-8).
 
+    Notes:
+     Linear interpolation is the default -- quadratic can lead to some
+     rather strange values. One should randomly sample positions and
+     verify the cummulative interpolation is working proplerly on
+     new maps.
+
     """
     def __init__(self, mapfile, seqlens, fill_to_end=True,
-                 cumm_interpolation='quadratic',
+                 cumm_interpolation='linear',
                  rate_interpolation='linear',
                  conversion_factor=1e-8):
         self.mapfile = mapfile
@@ -60,6 +66,16 @@ class RecMap(object):
         self._readmap()
 
     def _readmap(self):
+        """
+        Notes:
+         cummulative rates are calculated as:
+          |  r1  |    r2       |         r3     |
+          |  w1  |    w2       |         w3     |
+                 m1            m2              m3
+        c1: = w1*r1
+        c2: = w2*r2 + w1*r1
+        c3: = w3*r3 + w2*r2 + w1*r1
+        """
         rates = defaultdict(list)
         last_chrom, last_end = None, None
         first_bin = True
@@ -121,11 +137,15 @@ class RecMap(object):
             rate = np.array([r for _, r in data])
             rbp = rate * self.conversion_factor
             rates[chrom] = RecPair(pos, rbp)
-            widths = np.diff(pos)
-            cumrates = np.nancumsum(rbp[1:]*widths)
-            pad_cumrates = np.zeros(cumrates.shape[0]+1)
-            pad_cumrates[1:] = cumrates
-            cumm_rates[chrom] = RecPair(pos, pad_cumrates)
+            assert pos[0] == 0
+            # note: we exclude the first bin (0) -- the cummulative
+            # distances should start from the first marker onwards (otherwise
+            # there's a jump)
+            widths = np.diff(pos[1:])
+            cumrates = np.nancumsum(rbp[1:-1]*widths)
+            cumm_pos = pos[1:-1]
+            assert len(cumm_pos) == len(cumrates)
+            cumm_rates[chrom] = RecPair(cumm_pos, cumrates)
         self.rates = rates
         self.cumm_rates = cumm_rates
         self.cumm_interpol = rate_interpol(cumm_rates, kind=self.cumm_interpolation)