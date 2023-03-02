0e6ebc3778a4afc05843335b33a030c54e650746
Vince Buffalo <vsbuffalo@gmail.com>
2023-03-01 22:19:25 -0800

    sim stuff

--- bgspy/command_line.py
+++ bgspy/command_line.py
@@ -284,18 +284,18 @@ def data(seqlens, recmap, neutral, access, fasta,
 @click.option('--Bp-only', default=False, is_flag=True, help="only calculate B'")
 @click.option('--bs-file', required=True, type=click.Path(exists=True),
               help="BGSModel genome model pickle file (contains B' and B)")
+@click.option('--chrom', required=True, help="sim chromosome")
 @click.option('--output', default=None, required=True,
               type=click.Path(dir_okay=False, writable=True),
               help="pickle file for results")
-@click.option('--window',
-              help='size (in basepairs) of the window',
-              type=int, default=1_000_000)
+@click.option('--window', help='size (in basepairs) of the window',
+              type=int, required=True)
 def simdata(sim_tree, bs_file, output, window, sim_mu, bp_only):
     """
     Pre-process a tskit.TreeSequence simulated tree.
     """
     summarize_sim_data(sim_tree, bs_file, output,
-                       window, 'chr10', sim_mu, bp_only)
+                       window, sim_chrom, sim_mu, bp_only)
 
 
 @cli.command()
